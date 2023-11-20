3d808800b9168af13efaf7530cfea0449dbb55e0
Wickhye <jihyesim170@gmail.com>
2023-11-20 12:23:43 +0900

    add: dto 추기

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,25 @@
-export class Product {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  productId: number;
+
+  @Column()
+  productName: string;
+
+  @Column({ length: 1000 })
+  productDetail: string;
+
+  @Column()
+  price: number;
+
+  @Column({ default: true })
+  isInStock: boolean;
+
+  @Column({ default: 0 })
+  like: number;
+
+  @Column({ nullable: true })
+  imgUrl: string;
+}
