592f27f951045e6afd417fd11b46ccd1d5ae4420
Wickhye <jihyesim170@gmail.com>
2023-11-20 10:38:44 +0900

    add: database 환경변수 테스트

--- src/products/products.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('products')
-export class ProductsController {
-  @Get()
-  findAll(): string {
-    return 'this acrtion return all cats';
-  }
-}
