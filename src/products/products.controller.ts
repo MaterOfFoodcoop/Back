ba9b1bdd4a4776bcec6e08090b93eabdd55a82b0
Wickhye <jihyesim170@gmail.com>
2023-11-27 16:58:40 +0900

    add: multer 라이브러리

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,19 +6,23 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     this.productsService.create(createProductDto);
   }
 
+  // @UseGuards(AuthGuard('jwt'))
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
   //   return this.productsService.update(+id, updateProductDto);
@@ -39,6 +43,7 @@ export class ProductsController {
     return this.productsService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.productsService.remove(+id);
