2279fe6e17fed946eb38eaa326234f7f721fe68e
Wickhye <jihyesim170@gmail.com>
2023-11-28 09:13:03 +0900

    add: 이미지 보내기

--- src/products/ProductsController.ts
+++ src/products/ProductsController.ts
@@ -9,6 +9,9 @@ import {
   UseGuards,
   UseInterceptors,
   UploadedFiles,
+  StreamableFile,
+  NotFoundException,
+  HttpStatus,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -16,6 +19,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { join } from 'path';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import { createReadStream, existsSync } from 'fs';
 
 @Controller('products')
 export class ProductsController {
@@ -44,11 +48,29 @@ export class ProductsController {
     return files;
   }
 
+  @Get('download/:filename')
+  async downloadImg(
+    @Param('filename') filename: string,
+  ): Promise<StreamableFile> {
+    const filepath = join(__dirname, '..', '..', 'uploads', filename);
+
+    if (!existsSync(filepath)) {
+      throw new NotFoundException({
+        status: HttpStatus.NOT_FOUND,
+        message: 'File not found',
+      });
+    }
+
+    const file = createReadStream(filepath);
+    return new StreamableFile(file);
+  }
+
   // @UseGuards(AuthGuard('jwt'))
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
   //   return this.productsService.update(+id, updateProductDto);
   // }
+
   @Get()
   findAll() {
     return this.productsService.findAll();
