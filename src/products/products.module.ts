c4f0eab584ae1a1d9e72abccb7ea6a5b2e348b93
Wickhye <jihyesim170@gmail.com>
2023-12-04 15:01:25 +0900

    add: 가격, 재고 수정

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { Product } from './entities/product.entity';
-import { ProductsController } from './ProductsController';
 import { MulterModule } from '@nestjs/platform-express';
+import { ProductsController } from './products.controller';
 
 @Module({
   imports: [
