5fb5934615db16fabadf3a643aa51e895380d169
Wickhye <jihyesim170@gmail.com>
2023-12-04 15:57:30 +0900

    chore: 스웨거 설치 및 경로 이슈 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
 import { QnAModule } from './qna/qna.module';
@@ -16,7 +14,5 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [DatabaseModule, ProductsModule, QnAModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
